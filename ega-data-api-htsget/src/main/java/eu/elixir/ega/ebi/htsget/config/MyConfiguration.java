656dc6f45a7e5fc94bcfd897160e337caa6e9dc8
Oscar Martinez <oscar.martinez@crg.eu>
2019-01-21 18:51:09 +0100

    add profile to put the user info in headers like in dataedge

--- ega-data-api-htsget/src/main/java/eu/elixir/ega/ebi/htsget/config/MyConfiguration.java
+++ ega-data-api-htsget/src/main/java/eu/elixir/ega/ebi/htsget/config/MyConfiguration.java
@@ -16,7 +16,9 @@
 package eu.elixir.ega.ebi.htsget.config;
 
 import com.google.common.cache.CacheBuilder;
+import eu.elixir.ega.ebi.shared.config.ClientUserIpInterceptor;
 import eu.elixir.ega.ebi.shared.dto.MyExternalConfig;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -26,6 +28,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.backoff.FixedBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
@@ -62,6 +66,22 @@ public class MyConfiguration {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Profile("add-user-ip-headers")
+    @Bean
+    ClientUserIpInterceptor clientUserIpInterceptor() {
+        return new ClientUserIpInterceptor();
+    }
+
+    @Profile("add-user-ip-headers")
+    @Bean
+    @Primary
+    @LoadBalanced
+    public RestTemplate interceptedRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(Collections.singletonList(clientUserIpInterceptor()));
+        return restTemplate;
     }
 
     @Bean
