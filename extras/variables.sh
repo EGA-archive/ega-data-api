afa707d794707a7a6029dc148437303dbbe33f1e
amohan <amohan@W10-L-TRMZM2.windows.ebi.ac.uk>
2019-04-02 14:06:17 +0100

    Adding

---
+++ extras/variables.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export AWS_ACCESS_KEY="D0MmGMmrOLwxjEE5"
+export AWS_SECRET="PT6hqA1oaW65whte0y4NDMQ88KDCtBp9"
+export ENDPOINT_URL="http://130.239.81.206:9000" 
+export ENDPOINT_REGION="europe"
+
+export POSTGRES_SCHEMA="dev_ega_file"
+export POSTGRES_USER="testuser"
+export POSTGRES_PASSWORD="testpassw0rd"
+export POSTGRES_DB="lega"
+export JWTKEY="-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugd
+UWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQs
+HUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5D
+o2kQ+X5xK9cipRgEKwIDAQAB
+-----END PUBLIC KEY-----"
