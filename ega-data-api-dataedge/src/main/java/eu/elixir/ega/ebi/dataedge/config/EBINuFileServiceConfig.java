2f5ffdc2aec2fad91b4912bcbc9f1b66f790dbc8
Anna Foix <afoix@ebi.ac.uk>
2020-12-06 21:05:00 +0000

    Add missing beans and config

--- ega-data-api-dataedge/src/main/java/eu/elixir/ega/ebi/dataedge/config/EBINuFileServiceConfig.java
+++ ega-data-api-dataedge/src/main/java/eu/elixir/ega/ebi/dataedge/config/EBINuFileServiceConfig.java
@@ -21,17 +21,78 @@ import eu.elixir.ega.ebi.dataedge.service.FileMetaService;
 import eu.elixir.ega.ebi.dataedge.service.KeyService;
 import eu.elixir.ega.ebi.dataedge.service.NuFileService;
 import eu.elixir.ega.ebi.dataedge.service.internal.EBINuFileService;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.ac.ebi.ega.fire.properties.HttpClientProperties;
+import uk.ac.ebi.ega.fire.service.FireService;
 import uk.ac.ebi.ega.fire.service.IFireService;
+
+import java.util.Base64;
+import java.util.Collections;
 
 @Configuration
 public class EBINuFileServiceConfig {
+
+    @Value("${fire.user:testUser}")
+    private String fireUsername;
+    @Value("${fire.password:testPass}")
+    private String firePassword;
+    @Value("${fire.url:testUrl}")
+    private String fireURL;
 
     @Bean
     public NuFileService nuFileService(KeyService keyService,
                                        FileMetaService fileDatabase,
                                        IFireService fireService) {
         return new EBINuFileService(keyService, fileDatabase, fireService);
+    }
+
+    @ConfigurationProperties(prefix = "httpclient.connection")
+    @Bean
+    public HttpClientProperties initHttpClientProperties() {
+        return new HttpClientProperties();
+    }
+
+    @Bean
+    public CloseableHttpClient initHttpClient(final HttpClientProperties httpClientProperties) {
+
+        final ConnectionConfig connectionConfig = ConnectionConfig.custom()
+                .setBufferSize(httpClientProperties.getBufferSize())
+                .build();
+
+        final PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(httpClientProperties.getMaxTotal());
+        connectionManager.setDefaultMaxPerRoute(httpClientProperties.getDefaultMaxPerRoute());
+        connectionManager.setValidateAfterInactivity(5000);
+
+        final RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(httpClientProperties.getTimeout() * 1000)
+                .setConnectionRequestTimeout(httpClientProperties.getTimeout() * 1000)
+                .setSocketTimeout(0)
+                .build();
+
+        return HttpClients.custom()
+                .setDefaultConnectionConfig(connectionConfig)
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .setDefaultHeaders(Collections.singleton(new BasicHeader("Authorization", "Basic ".concat(base64EncodedCredentials()))))
+                .build();
+    }
+
+    @Bean
+    public IFireService initFireService(final CloseableHttpClient httpClient) {
+        return new FireService(httpClient, fireURL, 3);
+    }
+
+    private String base64EncodedCredentials() {
+        return Base64.getEncoder().encodeToString((fireUsername + ":" + firePassword).getBytes());
     }
 }
