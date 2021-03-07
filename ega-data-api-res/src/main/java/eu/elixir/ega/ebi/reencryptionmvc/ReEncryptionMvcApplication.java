474022791524e32d9821294b61e8fdd5eaa2e8be
anandmohan777 <anandmohan777@gmail.com>
2021-03-07 11:06:27 +0000

    Removing unused localEGA classes

--- ega-data-api-res/src/main/java/eu/elixir/ega/ebi/reencryptionmvc/ReEncryptionMvcApplication.java
+++ ega-data-api-res/src/main/java/eu/elixir/ega/ebi/reencryptionmvc/ReEncryptionMvcApplication.java
@@ -16,6 +16,7 @@
 package eu.elixir.ega.ebi.reencryptionmvc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -25,6 +26,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableCaching
 @EnableEurekaClient
+@SpringBootApplication
 public class ReEncryptionMvcApplication {
 
     public static void main(String[] args) {
--- ega-data-api-res/src/main/java/eu/elixir/ega/ebi/reencryptionmvc/rest/FileController.java
+++ ega-data-api-res/src/main/java/eu/elixir/ega/ebi/reencryptionmvc/rest/FileController.java
@@ -19,42 +19,22 @@ import eu.elixir.ega.ebi.reencryptionmvc.dto.ArchiveSource;
 import eu.elixir.ega.ebi.reencryptionmvc.exception.NotFoundException;
 import eu.elixir.ega.ebi.reencryptionmvc.service.ArchiveService;
 import eu.elixir.ega.ebi.reencryptionmvc.service.ResService;
-import htsjdk.samtools.seekablestream.RandomInputStream;
-import htsjdk.samtools.seekablestream.ebi.BufferedBackgroundInputStream;
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.math.BigInteger;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Iterator;
-import java.util.UUID;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 /**
  * @author asenf
  */
 @RestController
 @EnableDiscoveryClient
-@Slf4j
 @RequestMapping("/file")
 public class FileController {
 
-    /**
-     * Size of a byte buffer to read/write file (for Random Stream)
-     */
-    private static final int BUFFER_SIZE = 4096;
     private ResService resService; // Handle Any Direct Re/Encryption Operations
     private ArchiveService archiveService; // Handle Archived File Operations (file identified by Archive ID)
 
@@ -146,84 +126,6 @@ public class FileController {
         return source.getSize();
     }
 
-    // *************************************************************************
-    // *************************************************************************
-    // *************************************************************************
-    // ************************************************************************* Test Code - To be Removed!
-
-    // Generate a random input stream and return it as a Flux of byte[]
-    @GetMapping(value = "/test1/{size}")
-    public void getArchiveFile(@PathVariable("size") String size) {
-        RandomInputStream in = new RandomInputStream(Integer.parseInt(size));
-        BufferedBackgroundInputStream bbIn = new BufferedBackgroundInputStream(in);
-
-        Iterator<byte[]> sourceIterator = bbIn.iterator();
-
-        Iterable<byte[]> iterable = () -> sourceIterator;
-        Stream<byte[]> targetStream = StreamSupport.stream(iterable.spliterator(), false);
-
-        //return Flux.fromStream(targetStream);
-    }
-
-
-    // Return a stream of random bytes
-    @GetMapping(value = "/test2/{size}")
-    public String doDownload(@PathVariable("size") String size,
-                             @RequestParam(value = "random", required = false) boolean random,
-                             HttpServletRequest request,
-                             HttpServletResponse response) throws IOException, NoSuchAlgorithmException {
-
-        // get absolute path of the application
-        ServletContext context = request.getServletContext();
-        String appPath = context.getRealPath("");
-        log.info("appPath = " + appPath);
-
-        //String size = "1024";
-        InputStream inputStream = new RandomInputStream(Integer.parseInt(size));
-        //InputStream inputStream = new RandomInputStream(Integer.parseInt(size));
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        DigestInputStream dIn = new DigestInputStream(inputStream, md);
-
-        // get MIME type of the file
-        String mimeType = "application/octet-stream";
-
-        // set content attributes for the response
-        response.setContentType(mimeType);
-        //response.setContentLength(Integer.parseInt(size));
-
-        // Build Header - Specify UUID
-        UUID dlIdentifier = UUID.randomUUID();
-
-        // Set headers for the response
-        String headerKey = "X-Session";
-        String headerValue = dlIdentifier.toString();
-        response.setHeader(headerKey, headerValue);
-
-        // get output stream of the response
-        OutputStream outStream = response.getOutputStream();
-
-        byte[] buffer = new byte[BUFFER_SIZE];
-        int bytesRead = -1;
-
-        // write bytes read from the input stream into the output stream
-        while ((bytesRead = dIn.read(buffer)) != -1) {
-            outStream.write(buffer, 0, bytesRead);
-        }
-
-        dIn.close();
-        inputStream.close();
-        outStream.close();
-
-        byte[] digest_ = md.digest();
-        BigInteger bigInt_ = new BigInteger(1, digest_);
-        String hashtext_ = bigInt_.toString(16);
-        while (hashtext_.length() < 32) {
-            hashtext_ = "0" + hashtext_;
-        }
-
-        return hashtext_;
-
-    }
 
     @Autowired
     public void setResService(ResService resService) {
